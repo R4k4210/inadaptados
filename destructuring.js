7c2b5096306fcff3462ef1cc0ffdb97cc3ef1b86
R4k4210 <elchopi26@gmail.com>
2023-04-16 19:02:20 -0300

    feature: :sparkles: Cambios

--- destructuring.js
+++ destructuring.js
@@ -18,31 +18,68 @@
 let a, b, rest;
 [a, b] = [10, 20];
 
-console.log(a);
-console.log(b);
+// console.log(a);
+// console.log(b);
 
 [a, b, ...rest] = [10, 20, 30, 40, 50];
 
-console.log(rest);
+// console.log(rest);
 
-//También es una buena forma de realizar copias del contenido de una lista u
+// const obj1 = {
+// nombre: "santi",
+// apellido: "chopitea",
+// };
+
+// const obj2 = obj1;
+
+// console.log(obj1);
+// console.log(obj2);
+
+// obj1.apellido = "sanchez";
+//
+// console.log(obj1);
+// console.log(obj2);
+
+// También es una buena forma de realizar copias del contenido de una lista u
 //objetos (que no tenga un objeto dentro)
 
 const array1 = [5, 10, 20, "hola", false];
 const array2 = [...array1];
-console.log(array2);
+// console.log(array2);
+
+array1.push("perro");
+// console.log(array1);
+// console.log(array2);
 
 const obj1 = {
   nombre: "Mati",
   apellido: "Sanchez",
   edad: 40,
+  bicileta: {
+    marca: "venzo",
+    modelo: "phoenix",
+  },
 };
 
-const obj2 = { ...obj1 };
-console.log(obj2);
+const obj2 = { ...obj1 }; //ES UNA COPIA - Shallow copy
+// console.log(obj1);
+// console.log(obj2);
+
+obj1.nombre = "Agustin";
+obj1.bicileta.marca = "Santi";
+
+// console.log(obj1);
+// console.log(obj2);
+
+// DEEP COPY
+const obj3 = JSON.parse(JSON.stringify(obj1));
+
+// console.log(JSON.stringify(obj1));
+
+// UN DATO ESCALAR, son datos primitivos.
 
 const { edad } = { ...obj1 };
-console.log(edad);
+// console.log(edad);
 
 //--------------------------------------------------------------
 // Enviar una lista destructurada como parámetros de una función
@@ -53,13 +90,13 @@ const func1 = (nombre, apellido, edad) => {
 
 const funct2 = (nombre, apellido, edad, sexo, hijos) => {
   console.log(
-    `Mi también es nombre es ${nombre} ${apellido} y tengo ${edad} años`
+    `Mi nombre también es ${nombre} ${apellido} y tengo ${edad} años`
   );
   console.log(`Ademas mi sexo es ${sexo} y tengo ${hijos} hijos.`);
 };
 
 const params = ["Matias", "Sanchez", 40];
-const masParams = false;
+const masParams = true;
 
 if (masParams) {
   params.push("Masculino");
